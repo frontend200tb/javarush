e971c1ce824b90dfd4d99bef3a2b401c644b7567
frontend200tb <frontend200tb@mail.ru>
2024-11-07 19:48:29 +0300

    feat:2024.11.07

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const menu = [
+const questsLinks = [
   'quest1/quest1.html',
   'quest2/quest2.html',
   'quest3/quest3.html',
@@ -8,13 +8,38 @@ const menu = [
   'quest7/quest7.html',
 ]
 
-// Карточки
-const cards = document.querySelectorAll('.card');
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener('click', clickCard);
+const level1Links = [
+  'lec0.html',
+  'lec1.html',
+  'lec2.html',
+  'lec3.html',
+  'lec4.html',
+  'lec5.html',
+  'lec6.html',
+  'lec7.html',
+  'lec8.html',
+]
+
+// Карточки квестов
+const questCards = document.querySelectorAll('.js-quest');
+for (let i = 0; i < questCards.length; i++) {
+  questCards[i].addEventListener('click', clickQuest);
 }
 
-function clickCard(e) {
+function clickQuest(e) {
   e.preventDefault();
-  location.href = menu[e.currentTarget.dataset.id];
+  location.href = questsLinks[e.currentTarget.dataset.id];
 }
+// конец Карточки квестов
+
+// Карточки лекций уровня 1
+const level1Cards = document.querySelectorAll('.js-lec');
+for (let i = 0; i < level1Cards.length; i++) {
+  level1Cards[i].addEventListener('click', clickLec);
+}
+
+function clickLec(e) {
+  e.preventDefault();
+  location.href = level1Links[e.currentTarget.dataset.id];
+}
+// конец Карточки лекций уровня 1
